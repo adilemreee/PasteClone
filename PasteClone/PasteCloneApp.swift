d909bc7f2b7d7c111ed9c434cfae92f30cd91f27
Adil Emre <adil.emre@hotmail.com>
2026-01-12 15:04:40 +0300

    Initial Commit

---
+++ PasteClone/PasteCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasteCloneApp.swift
+//  PasteClone
+//
+//  Created by adil emre  on 12.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct PasteCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
